8cfe73a36e6201825bed932d03e307be36f4d262
Ouk Saksith <60955155+OukSaksith@users.noreply.github.com>
2024-03-22 08:29:42 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Jenkins so great !!!
+         Jenkins so great !!! Hi long
         </p>
         <a
           className="App-link"
